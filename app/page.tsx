a06c9b39caaac996e9216fb2a439bce32b3e409f
Florent <florent.vincerot@me.com>
2024-03-01 17:14:30 +0100

    dark mode ok

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+import { ModeToggle } from "@/components/ui/ModeToggle";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div>
+        <ModeToggle />
+      </div>
       <div className=" text-9xl">Hello</div>
       <div>
         <Button>Click me</Button>
